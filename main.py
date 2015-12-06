98a51b6ca21970c0ca0088452277f6bd2d09da08
henryfox <henryfox831@gmail.com>
2015-12-06 09:43:01 -0800

    setup reverse geocoder in the python

--- main.py
+++ main.py
@@ -24,20 +24,28 @@ class MainHandler(Handler):
 
 class replyhandler(Handler):
 	def get(self):
+		option = self.request.get("option")
 		adress = self.request.get("adress")
 		api_key_file = os.path.join(os.path.dirname(__file__), "secret.txt")
 		api_key = open(api_key_file)
 		api_key = api_key.read()
-		info = urlopen("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (adress, api_key))
+		if option == "geocode":
+			info = urlopen("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (adress, api_key))
+		if option == "reverse-geocode":
+			info = urlopen("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s&key=%s" % (adress, api_key))
 		info = info.read()
 		j = json.loads(info)
 		geometry = j["results"][0]["geometry"]["location"]
-		lat = geometry["lat"]
-		lng = geometry["lng"]
+		if option == "geocode": 
+			lat = geometry["lat"]
+			lng = geometry["lng"]
 		formated_adress = j["results"][0]["formatted_address"]
-		self.render("response.html", title="response", lat=lat, lng=lng, adress=formated_adress)
+		if option == "geocode":
+			self.render("response.html", title="response", lat=lat, lng=lng, adress=formated_adress)
+		if option == "reverse-geocode":
+			self.render("response_reverse.html", adress=formated_adress, title="response")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/coords', replyhandler)
+    ('/response', replyhandler),
 ], debug=True)
