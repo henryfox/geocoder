a721444c0db4cf706c9a0d32a1599fa53a255a44
henryfox <henryfox831@gmail.com>
2015-12-06 07:24:34 -0800

    add map

--- main.py
+++ main.py
@@ -24,17 +24,17 @@ class MainHandler(Handler):
 
 class replyhandler(Handler):
 	def get(self):
-		self.adress = self.request.get("adress")
+		adress = self.request.get("adress")
 		api_key_file = os.path.join(os.path.dirname(__file__), "secret.txt")
 		api_key = open(api_key_file)
 		api_key = api_key.read()
-		info = urlopen("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (self.adress, api_key))
+		info = urlopen("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (adress, api_key))
 		info = info.read()
 		j = json.loads(info)
 		geometry = j["results"][0]["geometry"]["location"]
 		lat = geometry["lat"]
 		lng = geometry["lng"]
-		self.render("response.html", title="response", lat=lat, lng=lng)
+		self.render("response.html", title="response", lat=lat, lng=lng, adress=formated_adress)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
